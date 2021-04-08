ed6dc89992ea5fb9baf65ec52b0857a56b398f3b
Fabien <fhannon60@gmail.com>
2021-04-08 11:58:58 +0200

    remote json url added

--- app/script.py
+++ app/script.py
@@ -1,16 +1,20 @@
-import json
 import os
-
-from flask import Flask, render_template
+from flask import Flask, render_template, current_app as app
+import json, urllib.request
 
 app = Flask(__name__)
 
 filename = os.path.join(app.static_folder, 'data', 'funnel_data.json')
 
+#url = "https://raw.githubusercontent.com/FabienH27/projet-d3js/main/data/funnel_data.json"
 
 @app.route('/')
 def hello_world():
     funnel_list = []
+    #Read from source
+    #with urllib.request.urlopen(url) as f:
+        #data = json.loads(f.read().decode('utf-8'))
+    #Read locally
     with open(filename) as f:
         data = json.load(f)
         for d in data:
